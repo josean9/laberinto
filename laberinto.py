882d9245ecfee4c6415b3a393fd7b7303d22db86
jose antonio <jsancesc@gmail.com>
2022-12-02 23:44:59 +0100

    laberintoTerminado

--- laberinto.py
+++ laberinto.py
@@ -8,11 +8,11 @@ laberinto = [
 muro = ((0,1), (0,2), (0,3), (0,4), (1,1), (2,1), (2,3), (3,3), (4,0), (4,1), (4,2), (4,3))
 
 
-def laberinto(dimension, muros):
+def laberinto(FilaColumna, muros):
     laberinto = []
-    for i in range(dimension):
+    for i in range(FilaColumna):
         fila = []
-        for j in range(dimension):
+        for j in range(FilaColumna):
             if tuple([i, j]) in muros:
                 fila.append('X')
             else:
